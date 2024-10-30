933c56d86f8a83f0dcdf9f2dd236923dd7ecd1a2
k0yaan1sq4tsi <k0yaan1sq4tsi@proton.me>
2024-10-30 16:00:08 +0000

    Update DOM-Auditor.py
    Deleting comments and formatting

--- DOM-Auditor.py
+++ DOM-Auditor.py
@@ -39,28 +39,22 @@ def parse_js_ast(js_content):
 # Track the flow of user input data
 def track_user_input_flow(ast, html_ids):
     vulnerabilities = []
-    user_input_vars = set()  # Track variables assigned with user input
-
+    user_input_vars = set() 
     # Helper to check if an expression is sanitized
     def is_sanitized(expr):
-        # Add known safe functions here (e.g., encodeURIComponent, escapeHTML)
+        # Add known safe functions here
         safe_functions = {'encodeURIComponent', 'escapeHTML'}
         return (expr['type'] == 'CallExpression' and 
                 expr['callee']['type'] == 'Identifier' and 
                 expr['callee']['name'] in safe_functions)
-
-    # Traverse the AST to identify user input sources
-    for node in ast['body']:
-        # Identify user input assignments (e.g., userInput = document.querySelector(...).value)
+    for node in ast['body']: 
         if node['type'] == 'VariableDeclaration':
             for decl in node['declarations']:
-                if decl['init'] and decl['init']['type'] == 'CallExpression':
-                    # Example: document.querySelector(...).value, location.search
+                if decl['init'] and decl['init']['type'] == 'CallExpression': 
                     if (decl['init']['callee']['type'] == 'MemberExpression' and 
                         decl['init']['callee']['object']['name'] in {'document', 'location'}):
                         user_input_vars.add(decl['id']['name'])
-
-    # Look for dangerous assignments involving `innerHTML`
+                            
     for node in ast['body']:
         if (node['type'] == 'ExpressionStatement' and 
             node['expression']['type'] == 'AssignmentExpression'):
@@ -69,7 +63,7 @@ def track_user_input_flow(ast, html_ids):
 
             if (left['type'] == 'MemberExpression' and 
                 left['property']['name'] == 'innerHTML'):
-                # Check if right side is unsanitized user input
+                
                 if (right['type'] == 'Identifier' and 
                     right['name'] in user_input_vars and 
                     not is_sanitized(right)):
